e38aa6debba6ea95f3d4ce724dc50e19bc62921d
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-11-28 17:10:36 +0500

    fixed the navbar button issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ export const Header = () => {
               Projects
             </button>
             <Link
-              to="/app/about/"
+              to="/app/about"
               className="text-foreground hover:text-accent transition-colors"
             >
               Ai-image generator
